20634882b6fd586bdc33da02a967260c6fdaa9ce
Ayushi katroliya <ayushikatroliya17h@gmail.com>
2024-07-30 19:43:07 +0530

    Logo change

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import logo from '../logo2.jpg'; // Adjust the path as needed
 
 export default function Navbar() {
   return (
-    <nav className='flex justify-between bg-blue-950 py-4'>
+    <nav className='flex justify-between items-center bg-blue-950 py-4'>
       <div className='flex px-4'>
-        <Link to="/" className='text-white font-bold text-2xl'>iTask</Link>
+        <Link to="/" className='flex items-center'>
+          <img src={logo} alt="iTask Logo" className="h-8 w-auto" /> {/* Adjust height as needed */}
+        </Link>
       </div>
       <ul className='flex px-8 gap-4 text-white text-xl'>
         <li className='cursor-pointer hover:font-bold transition-all'>
